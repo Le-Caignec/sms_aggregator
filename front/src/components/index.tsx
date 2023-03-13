062e35fb0b75ae1ebf2c738bc069d2eb6d041128
Robin <roleciq@gmail.com>
2023-03-13 09:24:54 +0100

    delete files

--- front/src/components/index.tsx
+++ front/src/components/index.tsx
@@ -1,5 +1,5 @@
-export {default as Card} from './card/Card';
-export {default as NavBar} from './navBar/NavBar';
-export {default as Secret} from './secret/Secret';
-export {default as ModalSecret} from './modal/ModalSecret';
-export {default as Mint} from './mint/Mint';
+export { default as Card } from './card/Card'
+export { default as NavBar } from './navBar/NavBar'
+export { default as Secret } from './secret/Secret'
+export { default as ModalSecret } from './modal/ModalSecret'
+export { default as Mint } from './mint/Mint'
